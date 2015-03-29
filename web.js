20f62dccf4717034581744e57641ebcef204691c
Calvin Hu <calvinhu00@gmail.com>
2015-03-29 15:30:18 -0700

    clean dist

--- web.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var http = require('http');
-var gzippo = require('gzippo');
-var logger = require('morgan');
-var app = express();
-app.use(logger());
-app.use(gzippo.staticGzip('' + __dirname));
-var server = http.createServer(app);
-server.listen(process.env.PORT || 5000);
